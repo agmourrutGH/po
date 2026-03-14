9a8db080e3a6dfb40a737fefa5ac1078307de55e
Agustin <agusmourrut@gmail.com>
2026-03-14 00:18:29 -0300

    ultimos cambios

--- src/data/en.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    heroTitle: "Full Stack Developer",
-    heroDesc:
-      "I build scalable web applications focused on performance and great user experience.",
-    viewProjects: "View projects",
-    contact: "Contact",
-  };
--- src/data/es.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    heroTitle: "Desarrollador Full Stack",
-    heroDesc:
-      "Construyo aplicaciones web escalables enfocadas en rendimiento y buena experiencia de usuario.",
-    viewProjects: "Ver proyectos",
-    contact: "Contacto",
-  };
